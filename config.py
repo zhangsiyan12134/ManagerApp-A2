378566352ea61188c2de713bfb82944b5df4c261
siyan <zhangsiyan12134@gmail.com>
2022-03-19 10:44:20 -0400

    Updated the instances private IP information

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    AUTOSCALAR_URL = "http://localhost:5004"
+    AUTOSCALAR_URL = "http://localhost:5003"
     FRONTEND_URL = "http://localhost:5000"
     SCHEDULER_API_ENABLED = True
     SCHEDULER_TIMEZONE = "America/Toronto"
@@ -22,7 +22,14 @@ class Config(object):
     }
     INSTANCE_LIST = {
         # a dictionary that contains AWS InstanceID and Private IP pairs
-        "i-07e8cb89884703402": "54.164.28.184"
+        "i-06ff465c20d0fb338": "172.31.19.26",
+        "i-0b81ca5b750466ce5": "172.31.28.166",
+        "i-04d4d716f1d677ddf": "172.31.21.22",
+        "i-0499cf05aa1ca5ddb": "172.31.20.212",
+        "i-08cf1f2da050d9e7c": "172.31.22.94",
+        "i-03d068ae131c3fd83": "172.31.25.107",
+        "i-0d700be59a58243d2": "172.31.21.0",
+        "i-002db696c9c99d3f5": "172.31.30.224"
     }
 
 
